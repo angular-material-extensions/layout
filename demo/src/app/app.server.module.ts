33d1430667975f698d6839f14795ba4e9f67e762
Anthony Nahas <anthony.na@hotmail.de>
2018-10-22 10:46:30 +0300

    fix(package): adjusted the static paths routes for the prerendered demo app

--- demo/src/app/app.server.module.ts
+++ demo/src/app/app.server.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import {NgModule} from '@angular/core';
+import {ServerModule, ServerTransferStateModule} from '@angular/platform-server';
+import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
 
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
+import {AppModule} from './app.module';
+import {AppComponent} from './app.component';
+import {FlexLayoutServerModule} from '@angular/flex-layout/server';
 
 @NgModule({
   imports: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
     // by the ServerModule from @angular/platform-server.
     AppModule,
     ServerModule,
+    FlexLayoutServerModule,
     ModuleMapLoaderModule,
     ServerTransferStateModule,
   ],
@@ -18,4 +20,5 @@ import { AppComponent } from './app.component';
   // imported AppModule, it needs to be repeated here.
   bootstrap: [AppComponent],
 })
-export class AppServerModule { }
+export class AppServerModule {
+}
--- demo/static.paths.ts
+++ demo/static.paths.ts
@@ -1,4 +1,11 @@
 export const ROUTES = [
-    '/',
+    '/home',
     '/getting-started/',
+    '/utilities/',
+    '/utilities/container',
+    '/utilities/margin',
+    '/utilities/padding',
+    '/utilities/width',
+    '/utilities/height',
+    '/layouts/',
 ];
